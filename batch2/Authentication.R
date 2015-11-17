d729b5bb6c16bb99d87e4a217cb3b83de1cbac87
unknown <qianghaoqiang@gmail.com>
2015-11-17 11:44:55 -0500

    Initialize

---
+++ batch2/Authentication.R
@@ -0,0 +1,42 @@
+# TwitterR was updated recently, which cause the change in authentication method
+# the following works with the latest version of TwitterR
+
+# load necessary packages
+if (require(ROAuth) == FALSE) {
+  install.packages("ROAuth")
+  library(ROAuth)
+}
+if (require(twitteR) == FALSE) {
+  install.packages("twitteR")
+  library(twitteR)
+}
+if (require(base64enc) == FALSE) {
+  install.packages("base64enc")
+  library(base64enc)
+}
+if (require(httpuv) == FALSE) {
+  install.packages("httpuv")
+  library(httpuv)
+}
+
+# consumer key and secret come from twitter
+# change the key and secret by the info of your twitter app
+consumer_key <- "uC9L8BG1PX9FVjfpVlqaKX5GC"
+consumer_secret <- "tWrpquA5sRg5GA6i1Vjlagpd2lYlKjTfSk6dgSI2ZPUQdatUuJ"
+
+# register function
+# input: consumer_key, consumer_secret
+register <- function(consumer_key, consumer_secret) {
+  # register
+  setup_twitter_oauth(consumer_key, consumer_secret)
+  ### save the authentication for future use
+  ## Setting working folder
+  # From windows machine in lab computer:
+  # setwd("D:\\R\\")
+  # From Mac computer, something like ~/Dropbox/
+  # setwd(folder_location)
+  # save the authentication token
+  # save(my_oauth, file="my_oauth")
+}
+
+register(consumer_key, consumer_secret)
---
+++ batch4/dataCollector.R
@@ -0,0 +1,30 @@
+#setwd("C:/Users/4malteses/Desktop/Data")
+
+## collectappend result to a csv file
+tweetCollect <- function(hashtag, numberOfTweets, nameOfFile) {
+  # search tweets by hashtag
+  tweets <- searchTwitter(hashtag, n = numberOfTweets)
+  # convert searchtwitter research into dataframe
+  tweets <- twListToDF(tweets)
+  # return results
+  tweets <- as.matrix(tweets$text)
+  write.table(tweets, file = nameOfFile, append = TRUE, sep = ",", col.names = FALSE,
+              qmethod = "double")
+}
+
+## collect tweet function
+dataCollector <- function(words, nameOfFile) {
+  number <- length(words)
+  for (i in 1: number) {
+    hashtag <- paste("#", words[i], sep="")
+    tweetCollect(hashtag, 500, nameOfFile)
+    Sys.sleep(61)
+  }
+}
+
+## read random words
+words <- read.csv("words4.csv", sep=" ", header = F, stringsAsFactors=FALSE)
+words <- words[,1]
+words <- as.character(words)
+
+dataCollector(words, "nosar4.csv")
