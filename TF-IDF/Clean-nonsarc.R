684328edccf4975baf10ba046fa85532b013d9b3
unknown <qianghaoqiang@gmail.com>
2015-12-15 12:39:07 -0500

    add tf-idf

---
+++ TF-IDF/Clean-nonsarc.R
@@ -0,0 +1,85 @@
+### data
+data <- read.csv("nonsarcastic-tweets.csv", header = T, stringsAsFactors =F)
+
+## detect url if possible
+reg <- "([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?"
+whetherLink <- grep(reg, data$text, ignore.case = FALSE)
+
+data.link <- rep(0, dim(data)[1])
+data.link[whetherLink] <- 1
+data <- cbind(data, data.link)
+names(data) <- c("text", "link")
+
+## remove url
+reg <- "([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?"
+data$text <- gsub(pattern = reg, replacement = "", data$text, ignore.case = T)
+
+## detect reply if possible
+reg <- "^@([a-zA-Z0-9]+://)?"
+whetherReply <- grep(reg, data$text, ignore.case = FALSE)
+
+data.rp <- rep(0, dim(data)[1])
+data.rp[whetherReply] <- 1
+data <- cbind(data, data.rp)
+names(data) <- c("text", "link", "reply")
+
+## remove @username
+reg <- "@+([a-zA-Z0-9_+-]+)"
+data$text <- gsub(pattern = reg, replacement = "", data$text, ignore.case = T)
+
+## remove \n
+reg <- "\n"
+data$text <- gsub(pattern = reg, replacement = "", data$text, ignore.case = T)
+
+## detect non character, non ASCII
+data$text <- gsub("[^0-9A-Za-z///' ]", "", data$text)
+
+# delete tweets with less than 5 chars
+len <- dim(data)[1]
+for (i in 1:len) {
+  if (nchar(data$text[i]) <= 8) {
+    data$text[i] <- NA
+  }
+}
+
+# remove NA
+data <- na.omit(data)
+
+
+# delete the exactly same tweets
+len <- dim(data)[1]
+count <- 1
+while (count < len) {
+  while (is.na(data$text[count])) {
+    count <- count + 1
+  }
+  
+  text.item <- data$text[count]
+  
+  for (i in (count+1):len) {
+    if (is.na(data$text[i])) {
+      next
+    }
+    
+    if (text.item == data$text[i]) {
+      data$text[i] <- NA
+    }
+  }
+  
+  count <- count + 1
+}
+
+# remove NA
+data <- na.omit(data)
+
+write.csv(data, "refined-subset1.csv", row.names = F)
+
+# save into three groups
+data.haveLinks <- data[data$link == 1,]
+data.isReply <- data[data$reply == 1,]
+data.rest <- data[data$link == 0,]
+data.rest <- data.rest[data.rest$reply == 0,]
+
+write.csv(data.haveLinks, "refined-subset-link.csv", row.names = F)
+write.csv(data.isReply, "refined-subset-reply.csv", row.names = F)
+write.csv(data.rest, "refined-subset-rest.csv", row.names = F)
---
+++ TF-IDF/convert - sarc.R
@@ -0,0 +1,108 @@
+###### figure out which document is empty
+library(tm)
+
+### bring in the data
+# load data
+#data <- read.csv("XXX", header = T, stringsAsFactors = F)
+data <- data$text
+len <- length(data)
+
+# trim white space just in case
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+data <- trim(data)
+
+# delete tweets with less than 5 chars
+for (i in 1:len) {
+  if (nchar(data[i]) <= 3) {
+    data[i] <- NA
+  }
+}
+
+# remove NA
+data <- na.omit(data)
+len <- length(data)
+
+# make a corpus
+docs <- Corpus(VectorSource(data))
+
+### remove sth.
+# remove punctuation
+docs <- tm_map(docs, removePunctuation)
+# remove numbers
+docs <- tm_map(docs, removeNumbers)
+# remove stop words
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# remove extra white splace
+docs <- tm_map(docs, stripWhitespace)
+
+### further process
+# lowercase
+docs <- tm_map(docs, tolower)
+# stem
+#library(SnowballC)
+#docs <- tm_map(docs, stemDocument)
+
+### finish pre-process
+# make it ready
+test.ready <- tm_map(docs, PlainTextDocument)
+dtm <- DocumentTermMatrix(test.ready)
+# rename rows
+rownames(dtm) <- 1:len
+# get the number of empty rows
+library(slam)
+rowTotals <- row_sums(dtm, na.rm = T)
+empty <- which(rowTotals == 0)
+empty <- as.vector(empty)
+
+
+
+###### read in data without empty rows
+empty <- -1 * empty
+data <- data[empty]
+len <- length(data)
+
+####### get tf-idf
+library(tm)
+
+# make a corpus
+docs <- Corpus(VectorSource(data))
+
+### remove sth.
+# remove punctuation
+docs <- tm_map(docs, removePunctuation)
+# remove numbers
+docs <- tm_map(docs, removeNumbers)
+# remove stop words
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# remove extra white splace
+docs <- tm_map(docs, stripWhitespace)
+
+### further process
+# lowercase
+docs <- tm_map(docs, tolower)
+# stem
+#library(SnowballC)
+#docs <- tm_map(docs, stemDocument)
+
+### add meta info
+# meta(docs, "tag") <- 1:983
+
+
+### finish pre-process
+# make it ready
+test.ready <- tm_map(docs, PlainTextDocument)
+dtm <- DocumentTermMatrix(test.ready,
+                          control = list(weighting = function(x) weightTfIdf(x, normalize = T),
+                                         stopwords = TRUE))
+# rename rows
+rownames(dtm) <- 1:len
+dtm <- removeSparseTerms(dtm, 0.9999)
+
+
+
+### tf-idf
+write.csv(as.matrix(dtm), file="tfidf-sarcastic.csv")
+
+
+
+
